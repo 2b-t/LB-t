6714fed5013a14b7b9f4c14e2cbfdd3868c788ee
2b-t <t.flatscher@hotmail.de>
2022-01-23 20:21:51 +0100

    feat: Added basic Makefile for minimal interface without VTK and nlohmann-json

---
+++ src/minimal_main.cpp
@@ -0,0 +1,18 @@
+/**\file     main.cpp
+ * \mainpage Mainpage of the LB-t computational fluid dynamics solver based on the incompressible Lattice-Boltzmann method
+ * \author   Tobit Flatscher (github.com/2b-t)
+*/
+
+#include <cstdlib>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+
+int main(int argc, char* argv[]) {
+  std::cout << "Test" << std::endl;
+  return EXIT_SUCCESS;
+}
