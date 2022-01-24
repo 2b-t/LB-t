1b4bea74de88ba59342cd12827f8de0abf3a3d56
2b-t <t.flatscher@hotmail.de>
2022-01-24 01:09:31 +0100

    feat: Added comments for next step with static interface

--- src/simple_main.cpp
+++ src/simple_main.cpp
@@ -21,6 +21,7 @@ int main(int argc, char* argv[]) {
   using T = double;
   using Lattice = lbt::lattice::D2Q9<T>;
   // using CollisionOperator = lbt::collision::Bgk
+  // Solver settings: Microscopic velocity: Set manually
 
   constexpr auto length = 1.2_m;
   constexpr auto velocity = 1.7_mps;
@@ -30,8 +31,13 @@ int main(int argc, char* argv[]) {
   constexpr auto density = lbt::material::Air::density(temperature, pressure);
   constexpr auto kinematic_viscosity = lbt::material::Air::kinematic_viscosity(temperature, pressure);
 
+  // Compute Reynolds number as well as conversion factors and pass them to the population class
+
   auto& omp_manager = lbt::OpenMpManager::getInstance();
+  //omp_manager.setThreadsNum(2);
   std::cout << omp_manager << std::endl;
+
+  // Geometry: Determine boundary conditions either from analytical geometry description
 
   // Create continuum
 
